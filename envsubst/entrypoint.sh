ad18756203755b2c1eeea8bae0f92e9bb37d235a
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2024-03-06 01:24:42 -0300

    fix: workflow

--- envsubst/entrypoint.sh
+++ envsubst/entrypoint.sh
@@ -4,4 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+if [ "${INPUT_TEMPLATE_FILE}" = "${INPUT_RESULT_FILE}" ]; then
+  echo 'Error: template file and result file cannot be the same.' >&2
+  exit 1
+fi
+
 envsubst < "${INPUT_TEMPLATE_FILE}" > "${INPUT_RESULT_FILE}"
