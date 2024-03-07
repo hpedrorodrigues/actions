580f5f9e8bf2e3741668028b6da86a3128a0b438
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2024-03-06 22:26:27 -0300

    Fixing entrypoint and workflow

--- envsubst/entrypoint.sh
+++ envsubst/entrypoint.sh
@@ -4,19 +4,30 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-IFS=' ' read -r -a template <<<"${INPUT_TEMPLATE}"
-IFS=' ' read -r -a result <<<"${INPUT_RESULT}"
+set -x
 
-if [ "${#template[@]}" -ne "${#result[@]}" ]; then
+template_size=0
+for item in ${INPUT_TEMPLATE}; do
+  eval template_item_${template_size}="${item}"
+  template_size=$((template_size + 1))
+done
+
+result_size=0
+for item in ${INPUT_RESULT}; do
+  eval result_item_${result_size}="${item}"
+  result_size=$((result_size + 1))
+done
+
+if [ "${template_size}" -ne "${result_size}" ]; then
   echo >&2 'Error: `template` and `result` must have the same number of items.'
-  echo >&2 "Got: template items=${#template[@]}, result items=${#result[@]}"
+  echo >&2 "Got: template items=${template_size}, result items=${result_size}"
   exit 1
 fi
 
-for i in "${!template[@]}"; do
-  template_file="${template[i]}"
-  result_file="${result[i]}"
+for i in $(seq '0' "$((template_size - 1))"); do
+  template_item="$(eval echo "\$template_item_${i}")"
+  result_item="$(eval echo "\$result_item_${i}")"
 
-  echo "Processing \"${template_file}\" -> \"${result_file}\""
-  envsubst <"${template_file}" >"${result_file}"
+  echo "Processing \"${template_item}\" -> \"${result_item}\""
+  envsubst <"${template_item}" >"${result_item}"
 done
