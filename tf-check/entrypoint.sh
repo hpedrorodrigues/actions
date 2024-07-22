ba7ea8762bca00511ff210369a0c725978e8f3de
Pedro Rodrigues <hpedrorodrigues@users.noreply.github.com>
2024-07-22 19:51:33 -0300

    Removing flags for `diff`s

--- tf-check/entrypoint.sh
+++ tf-check/entrypoint.sh
@@ -7,6 +7,6 @@ if ${VERBOSE:-false}; then
   set -o xtrace
 fi
 
-terraform fmt -check -diff -recursive \
-  && tofu fmt -check -diff -recursive \
-  && terragrunt hclfmt --terragrunt-check --terragrunt-diff
+terraform fmt -check -recursive \
+  && tofu fmt -check -recursive \
+  && terragrunt hclfmt --terragrunt-check
